1f5d8f19f358a4ac18cea5919a21e2b9ef06a7c7
Nasir Ahmed <ahsannasirahmed@gmail.com>
2024-01-03 21:10:06 +0600

    3

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
+function getPin(){
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4 ){
+        return pin;
+
+    }
+    else{
+        getPin();
+    }
+}
 function generatePin (){
     const random = Math.round(Math.random()*10000);
     return random;
-
 }
 
-console.log('added app js');
+document.getElementById('generate-pin').addEventListener('click', function(){
+    const pin = getPin();
+  
+        const displayPinField = document.getElementById('display-pin');
+        displayPinField.value = pin;
+})
